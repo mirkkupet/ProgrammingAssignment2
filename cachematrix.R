c27fd83a5e1986bc10aadc020f672ee90b82de90
mirkkupet <mirjamaria.petajaniemi@gmail.com>
2018-07-13 12:36:02 +0300

    My solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First we make a special matrix with makeCacheMatrix to be able to cache the inverse.
+## With function cacheSolve we return the cache of inverse if it has already been solved.
 
-## Write a short comment describing this function
+## With this function we can set and get the value of the matrix and the inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x) {
+	s <- NULL
+	set <- function(y) {
+		x <<- y
+		s <<- NULL
+	}
+	get <- function() x
+	setsolve <- function(solve) s <<- solve
+	getsolve <- function() s
+	list(set = set, get = get,
+		setsolve = setsolve,
+		getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## With this function we calculate the inverse of the matrix, except if it has already been calculated.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	s <- x$getsolve()
+	if(!is.null(s)) {
+		message("getting cached data")
+		return(s)
+	}
+	data <- x$get()
+	s <- solve(data, ...)
+	x$setsolve(s)
+	s
 }
